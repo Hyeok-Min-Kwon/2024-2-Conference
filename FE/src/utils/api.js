1261826931b1ad28a712366115e2925f0d373168
Hyeok-Min-Kwon <prun000111@gmail.com>
2025-02-13 17:29:32 +0900

    change FE code

--- FE/src/utils/api.js
+++ FE/src/utils/api.js
@@ -2,9 +2,20 @@ import axios from 'axios';
 
 export const askQuestion = async (question) => {
   try {
-    const response = await axios.post('http://localhost:5000/api/ask', { question });
+    const response = await axios.post(
+      "http://4.217.250.63:8080/api/ask", 
+      { question },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        withCredentials: true,
+        timeout: 30000
+      }
+    );
     return response.data.answer;
   } catch (error) {
+    console.error("Axios Error", error.response ? error.response.data : error.message);
     throw new Error('Error fetching answer');
   }
 };
